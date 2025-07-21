e68d605130f7fbc3dada54fdd4cc3f73c17a921e
YosifElessawi <yosifelessawi@yahoo.com>
2025-07-22 01:51:43 +0300

    frontend init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-red-500 text-2xl">
+        Hello world
+      </div>
+    </>
+  )
+}
+
+export default App
