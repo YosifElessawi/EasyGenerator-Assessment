a8717a1bac5f3bb83a4ffb13c0e473827813d24b
YosifElessawi <yosifelessawi@yahoo.com>
2025-07-22 21:32:57 +0300

    Strategies, guards, decorators

---
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { AuthService, JwtPayload } from '../auth.service';
+import { UserDocument } from '../../users/schemas/user.schema';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private authService: AuthService,
+    private configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET') || '1D', // TODO: Replace with proper secret management
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<UserDocument> {
+    const user = await this.authService.validateUserById(payload.sub);
+    
+    if (!user) {
+      throw new UnauthorizedException('Token is invalid');
+    }
+    
+    return user;
+  }
+}
